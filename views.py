ed4c3a4f57e869c4c8ac7570b77bb2a15ce77c2f
Naveen Kumar N <naveenandroid6557@gmail.com>
2020-04-03 14:40:35 +0530

    three

--- views.py
+++ views.py
@@ -12,3 +12,17 @@ class ProductOperations(View):
         ps = ProductSerializer(qs,many=True)
         json_data = JSONRenderer().render(ps.data)
         return HttpResponse(json_data,content_type="application/json")
+    
+    def post(self,request):
+        byte_data = request.body # Will get data in bytes
+        stm = io.BytesIO(byte_data) # converting bytes into streamed data
+        dict_data = JSONParser().parse(stm) # Converting streamed data into dictionary
+        ps = ProductSerializer(data=dict_data)
+        if ps.is_valid():
+            ps.save()
+            message = {"message":"Product is Saved"}
+        else:
+            message = {"errors":ps.errors}
+
+        json_data = JSONRenderer().render(message)
+        return HttpResponse(json_data,content_type="application/json")
