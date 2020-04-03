ecb1895cdda36a53eacacb16173f41ee707392d8
Naveen Kumar N <naveenandroid6557@gmail.com>
2020-04-03 14:40:00 +0530

    two

--- views.py
+++ views.py
@@ -5,3 +5,10 @@ from rest_framework.parsers import JSONParser
 from app3.serializers import ProductSerializer
 from app3.models import ProductModel
 import io
+
+class ProductOperations(View):
+    def get(self,request):
+        qs = ProductModel.objects.all()
+        ps = ProductSerializer(qs,many=True)
+        json_data = JSONRenderer().render(ps.data)
+        return HttpResponse(json_data,content_type="application/json")
